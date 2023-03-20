2aefeedb3baa5a12dbfe55f1c733e762efef4f50
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-03-20 11:06:01 +0700

    update

--- nodejs/config/connectDB.js
+++ nodejs/config/connectDB.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-// const DB_NAME = process.env.DB_NAME;
-const DB_NAME = 'anthome_db';
-// const DB_USER = process.env.DB_USER;
-const DB_USER = 'adminfpoly';
-// const DB_PASS = process.env.DB_PASS;
-const DB_PASS = '12345';
-// const DB_HOST = process.env.DB_HOST;
-const DB_HOST = '45.77.248.162';
+const DB_NAME = process.env.DB_NAME;
+// const DB_NAME = 'anthome_db';
+const DB_USER = process.env.DB_USER;
+// const DB_USER = 'adminfpoly';
+const DB_PASS = process.env.DB_PASS;
+// const DB_PASS = '12345';
+const DB_HOST = process.env.DB_HOST;
+// const DB_HOST = '45.77.248.162';
 
 // asm-angular
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
