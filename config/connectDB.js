9225d45eb655ce9ea44e3aae6b0df67bdffcef8a
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-03-20 14:03:08 +0700

    ok

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,13 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-// const DB_NAME = process.env.DB_NAME;
-const DB_NAME = 'anthomex_anthome';
-// const DB_USER = process.env.DB_USER;
-const DB_USER = 'anthomex_khiemntps16018';
-// const DB_PASS = process.env.DB_PASS;
-const DB_PASS = 'YL7riBuu2sdJ4Qy';
-// const DB_HOST = process.env.DB_HOST;
-const DB_HOST = '103.200.23.120';
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
 
 // asm-angular
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
